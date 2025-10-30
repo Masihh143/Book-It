dcb17faa13120efdadbbaf34895191c93c1b6209
masihh143 <masihuddin4545@gmail.com>
2025-10-30 21:04:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("✅ FoundIt API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on port ${PORT}`);
+});
